eb7f8e4866960b35f18e31740d5593086d232440
Rosslni <anderson.silvarossini@gmail.com>
2025-09-03 14:25:59 -0300

    Ver 0.2

---
+++ JavaSript/index.js
@@ -0,0 +1,15 @@
+function cadastroSalas() {
+  alert("Cadastro de salas em desenvolvimento!");
+}
+
+function realizarAgendamento() {
+  alert("Agendamento em desenvolvimento!");
+}
+
+function consultarAgendamentos() {
+  alert("Consulta de agendamentos em desenvolvimento!");
+}
+
+function consultarCadastros() {
+  alert("Consulta de cadastros em desenvolvimento!");
+}
