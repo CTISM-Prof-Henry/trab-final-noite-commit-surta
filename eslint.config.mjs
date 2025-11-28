381825ffb1b5c499bdcdf49ceb2f2366fbf03dbf
Rossini04 <anderson.silvarossini@gmail.com>
2025-11-28 12:50:29 -0300

    Testes v2
    Testes a partir dos que estavam de exemplo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,62 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        // jQuery
+        $: "readonly",
+        jQuery: "readonly",
+        // Node.js/CommonJS
+        module: "readonly",
+        require: "readonly",
+        exports: "writable",
+        // Variáveis globais do projeto
+        salasCadastradas: "writable",
+        agendamentos: "writable",
+        salasPorBloco: "readonly",
+        dataHandler: "readonly",
+        // Funções do logica.js
+        validarCadastroSala: "readonly",
+        adicionarSala: "readonly",
+        removerSala: "readonly",
+        validarAgendamento: "readonly",
+        adicionarAgendamento: "readonly",
+        removerAgendamento: "readonly",
+        filtrarSalas: "readonly",
+        filtrarAgendamentos: "readonly",
+        ordenarAgendamentos: "readonly",
+        verificarPeriodo: "readonly",
+        getDiaSemana: "readonly",
+        gerarCorAleatoria: "readonly",
+        // Funções do persistencia.js
+        carregarSalasCadastradas: "readonly",
+        salvarSalasCadastradas: "readonly",
+        carregarAgendamentos: "readonly",
+        salvarAgendamentos: "readonly",
+        // Funções do dom.js
+        atualizarTabelaSemanal: "readonly",
+        cadastroSalas: "readonly",
+        abrirModalAgendamento: "readonly",
+        consultarCadastros: "readonly",
+        filtrarCadastros: "readonly",
+        atualizarSalas: "readonly",
+        excluirSala: "readonly",
+        excluirAgendamento: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { 
+        "varsIgnorePattern": "^(cadastroSalas|abrirModalAgendamento|consultarCadastros|filtrarCadastros|atualizarSalas|excluirSala|excluirAgendamento|salvarSalasCadastradas|salvarAgendamentos|dataHandler)$",
+        "argsIgnorePattern": "^_|event"
+      }]
+    }
+  }
+];
